1cff642aee833e788d1966fb83d9bd382e98c9e2
Cole H <nightbat13131@gmail.com>
2025-04-18 10:13:10 -0700

    Final upload to GIT before getting screenshots.

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ require_once "config.php";
 require_once "utilities.php";
 
 func_redirect(PAGE_LANDING);
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +11,6 @@ func_redirect(PAGE_LANDING);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
     <title>Index</title>
     <!--<script scr="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
